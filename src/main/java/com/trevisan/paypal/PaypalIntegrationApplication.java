59a41f8b398bd7f285b2891fa6afcf89d8ed528c
Diego Trevisan <diego.trevisan@saamauditoria.com.br>
2024-10-28 14:50:23 -0300

    created paypal service and controller

--- src/main/java/com/trevisan/paypal/PaypalIntegrationApplication.java
+++ src/main/java/com/trevisan/paypal/PaypalIntegrationApplication.java
@@ -1,11 +1,11 @@
 package com.trevisan.paypal;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaypalIntegrationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PaypalIntegrationApplication.class, args);
 	}
---
+++ src/main/java/com/trevisan/paypal/paypal/PaypalController.java
@@ -0,0 +1,47 @@
+package com.trevisan.paypal.paypal;
+
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class PaypalController {
+    private final PaypalService paypalService;
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @PostMapping("/payment/create")
+    public RedirectView createView() {
+        try {
+            String cancelUrl = "http://localhost:8080/payment/cancel";
+            String successUrl = "http://localhost:8080/payment/success";
+
+            Payment payment = paypalService.createPayment(10.0,
+                    "USD",
+                    "paypal",
+                    "sale",
+                    "Payment description",
+                    cancelUrl,
+                    successUrl);
+            for (Links links : payment.getLinks()) {
+                if (links.getRel().equals("approval_url")) {
+                    return new RedirectView(links.getHref());
+                }
+            }
+        } catch (PayPalRESTException e) {
+            log.error("Error ocurred:: ", e);
+        }
+        return new RedirectView("/payment/error");
+    }
+}
