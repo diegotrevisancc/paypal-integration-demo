0a9d1e04c8a78766eff2a048cd5bbc9e5a5b244b
Diego Trevisan <diego.trevisan@saamauditoria.com.br>
2024-10-28 15:10:19 -0300

    finished controller

--- src/main/java/com/trevisan/paypal/paypal/PaypalController.java
+++ src/main/java/com/trevisan/paypal/paypal/PaypalController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -43,5 +44,30 @@ public class PaypalController {
             log.error("Error ocurred:: ", e);
         }
         return new RedirectView("/payment/error");
+    }
+
+    @GetMapping("/payment/success")
+    public String paymentSuccess(@RequestParam("paymentId") String paymentId,
+                                 @RequestParam("payerId") String payerId) {
+        try {
+            Payment payment = paypalService.executePayment(paymentId, payerId);
+            if (payment.getState().equals("approved")) {
+                return "paymentSuccess";
+            }
+        } catch (PayPalRESTException e) {
+            log.error("Error ocurred:: ", e);
+        }
+        return "paymentSuccess";
+
+    }
+
+    @GetMapping("/payment/cancel")
+    public String paymentCancel() {
+        return "paymentCancel";
+    }
+
+    @GetMapping("/payment/error")
+    public String paymentError() {
+        return "paymentError";
     }
 }
